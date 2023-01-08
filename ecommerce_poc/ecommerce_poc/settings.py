6cbd4480735554d37da7231ceaec58fed0e78a08
takingupspace <belllabenthusiast@gmail.com>
2023-01-08 09:54:43 -0600

    init; debug=false and allowed hosts set to domain and box ip

--- ecommerce_poc/ecommerce_poc/settings.py
+++ ecommerce_poc/ecommerce_poc/settings.py
@@ -16,16 +16,10 @@ from decouple import config
 import os
 import environ
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, True)
-)
-
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -34,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '66.228.52.101', 'sauerwebsites.com' ]
 
 
 # Application definition
