4f797a92d0a76e096b0895c0fda6ff1019d73c58
takingupspace <belllabenthusiast@gmail.com>
2023-01-12 13:07:00 -0600

    fixed debug check in all stripe product & price creation to pull from env

--- ecommerce_poc/ecommerce/management/commands/create_stripe_kitchen_and_home_appliance_products.py
+++ ecommerce_poc/ecommerce/management/commands/create_stripe_kitchen_and_home_appliance_products.py
@@ -1,9 +1,23 @@
-from decouple import config
 from ecommerce.models import KitchenAndHomeAppliance
 from django.core.management.base import BaseCommand
 from django.db import transaction
+import json
+from decouple import config as dev_config
+from django.conf import settings
 import stripe
-stripe.api_key = config('STRIPE_SECRET_KEY')
+# NOTE: we are only going to use json config files in the future
+# this is how all configuration imports will work
+
+if settings.DEBUG == False:
+  pass
+else:
+  with open('/etc/ecommerce_config.json') as config_file:
+    config = json.load(config_file)
+
+if settings.DEBUG == False:
+  stripe.api_key = dev_config("STRIPE_SECRET_KEY")
+else:
+  stripe.api_key = config["STRIPE_SECRET_KEY"]
 
 class Command(BaseCommand):
 
