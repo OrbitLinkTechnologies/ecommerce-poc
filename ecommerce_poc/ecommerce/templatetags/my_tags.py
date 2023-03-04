51c864ce675769c71557395cab19a3c3e6df3347
takingupcyberspace <tsauer91cs@gmail.com>
2023-03-04 09:04:01 -0600

    ready to test on live ec2

--- ecommerce_poc/ecommerce/templatetags/my_tags.py
+++ ecommerce_poc/ecommerce/templatetags/my_tags.py
@@ -5,13 +5,13 @@ from decouple import config as dev_config
 from django.conf import settings
 import stripe
 
-if settings.DEBUG == True:
+if settings.DEBUG == False:
   pass
 else:
   with open('/etc/ecommerce_config.json') as config_file:
     config = json.load(config_file)
 
-if settings.DEBUG == True:
+if settings.DEBUG == False:
   stripe.api_key = dev_config("STRIPE_SECRET_KEY")
 else:
   stripe.api_key = config["STRIPE_SECRET_KEY"]
