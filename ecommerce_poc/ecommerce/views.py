a4676a211ab251ef69a4a29cfa3d3bec75e334a9
takingupcyberspace <tsauer91cs@gmail.com>
2023-03-03 20:30:35 -0600

    ready to test on live ec2

--- ecommerce_poc/ecommerce/views.py
+++ ecommerce_poc/ecommerce/views.py
@@ -21,13 +21,13 @@ import stripe
 # NOTE: we are only going to use json config files in the future
 # this is how all configuration imports will work
 
-if settings.DEBUG == True:
+if settings.DEBUG == False:
   pass
 else:
   with open('/etc/ecommerce_config.json') as config_file:
     config = json.load(config_file)
 
-if settings.DEBUG == True:
+if settings.DEBUG == False:
   stripe.api_key = dev_config("STRIPE_SECRET_KEY")
 else:
   stripe.api_key = config["STRIPE_SECRET_KEY"]
@@ -629,7 +629,7 @@ class CreateCheckoutSessionView(View):
       )
     # the following domain is just a placeholder
     domain = 'https://ecommerce.sauerwebdev.com'
-    if settings.DEBUG == True:
+    if settings.DEBUG == False:
       domain = 'http://3.89.21.130:8000'
     checkout_session = stripe.checkout.Session.create(
       expand = ['line_items'], # this is supposed to give us access to line
@@ -713,7 +713,7 @@ def contact_us_page(request):
   return render(request, 'ecommerce/contact_us_page.html')
 
 def send_customer_question(request):
-  if settings.DEBUG == True:
+  if settings.DEBUG == False:
     from_email= dev_config("FROM_EMAIL")
     to_emails= dev_config("TO_EMAIL")
   else:
@@ -728,7 +728,7 @@ def send_customer_question(request):
     'Message Body: <br><br>' + request.POST['message']
   )
   try:
-    if settings.DEBUG == True:
+    if settings.DEBUG == False:
       sg = SendGridAPIClient(dev_config("SENDGRID_API_KEY"))
     else:
       sg = SendGridAPIClient(config["SENDGRID_API_KEY"])
@@ -742,7 +742,7 @@ def send_customer_question(request):
   
   # if customer wants a copy, send them an email as well
   if request.POST.get('mail_customer_a_copy'):
-    if settings.DEBUG == True:
+    if settings.DEBUG == False:
       from_email= dev_config("FROM_EMAIL")
     else:
       from_email= config["FROM_EMAIL"]
@@ -756,7 +756,7 @@ def send_customer_question(request):
       'Message Body: <br><br>' + request.POST['message']
     )
     try:
-      if settings.DEBUG == True:
+      if settings.DEBUG == False:
         sg = SendGridAPIClient(dev_config("SENDGRID_API_KEY"))
       else:
         sg = SendGridAPIClient(config["SENDGRID_API_KEY"])
@@ -776,7 +776,7 @@ def contact_us_page_success(request):
     })
 
 def send_customer_invoice(request, invoice_item):
-  if settings.DEBUG == True:
+  if settings.DEBUG == False:
     from_email = dev_config("FROM_EMAIL")
   else:
     from_email = config["FROM_EMAIL"]
@@ -791,7 +791,7 @@ def send_customer_invoice(request, invoice_item):
     ' Please feel free to utilize an open-source vulnerability scanner such as https://virustotal.com to confirm that there are no threats.'
   )
   try:
-    if settings.DEBUG == True:
+    if settings.DEBUG == False:
       sg = SendGridAPIClient(dev_config("SENDGRID_API_KEY"))
     else:
       sg = SendGridAPIClient(config["SENDGRID_API_KEY"])
